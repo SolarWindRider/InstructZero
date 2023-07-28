6d0340e01fc18e24bef72f7e171b90c5051be276
solarwindrider <1149902816@qq,com>
2023-07-28 16:46:07 +0800

    debug

--- InstructZero/experiments/run_instructzero.sh
+++ InstructZero/experiments/run_instructzero.sh
@@ -2,10 +2,10 @@ export CUDA_VISIBLE_DEVICES=7
 SFT=5
 RANDOM_PROJ='uniform'
 INTRINSIC_DIM=10
-# model_dir='lmsys/vicuna-13b-v1.3'
-# MODEL_NAME='vicuna'
-model_dir='WizardLM/WizardLM-13B-V1.1'
-MODEL_NAME='wizardlm' 
+model_dir='lmsys/vicuna-7b-v1.3'
+MODEL_NAME='vicuna'
+# model_dir='WizardLM/WizardLM-13B-V1.1'
+# MODEL_NAME='wizardlm' 
 
 datasets=(antonyms cause_and_effect common_concept diff)
 
